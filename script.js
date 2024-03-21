78750c9d42b96c654c399e3895e0c11d801dc35e
Mohammad Shezaan Ansari <shezaanansari2210@gmail.com>
2024-03-21 17:10:20 +0530

    gsap animations

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+// const { default: gsap } = require("gsap");
 
+// const { Power4 } = require("gsap");
+function homepage() {
+    
 gsap.set('.pinning', {scale: 5});
 var tl = gsap.timeline({
     scrollTrigger:{
@@ -29,18 +33,47 @@ tl.to('.rgt',{
     ease: Power4,
     xPercent: 10
 }, 'b');
+}
 
-gsap.to('.slide',{
-    scrollTrigger:{
-        trigger: '.talk',
-        start: 'top top',
-        end: 'bottom top',
-        pin: true,
-        scrub: 2,
-        overflow: 'auto',
-        markers: true
-    },
-    xPercent:-290,
-    
-})
+function talkpage(){
+    gsap.to('.slide',{
+        scrollTrigger:{
+            trigger: '.talk',
+            start: 'top top',
+            end: 'bottom top',
+            pin: true,
+            scrub: 2,
+            overflow: 'auto',
+        },
+        xPercent:-290,
+        
+    })
+}
 
+function teampage(){
+    document.querySelectorAll('.containers').forEach((elem)=>{
+        elem.addEventListener('mousemove',function(dets){
+           
+            gsap.to(this.querySelector('.image'), { opacity: 1, duration: .5, scale: 1, x:  gsap.utils.mapRange(0, window.innerWidth, -200, 200, dets.clientX), y: gsap.utils.mapRange(0, window.innerHeight, -100, 100, dets.clientY) , ease: Power4})
+        });
+        elem.addEventListener('mouseleave',function(dets){
+            gsap.to(this.querySelector('.image'), { opacity: 0, duration: .5, scale: 0.8, ease: Power4})
+        });
+        })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+homepage();
+talkpage();
+teampage();
